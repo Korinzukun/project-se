04925e27a7418579873139894685c465bf9d7e7c
Collins Githuku <kigutacollingithuku076@gmail.com>
2025-06-02 14:53:55 +0300

    Create initial HTML content

---
+++ script.js
@@ -0,0 +1,6 @@
+// Create an h2 element
+const h2 = document.createElement("h2");
+h2.textContent = "This content added by JavaScript";
+
+// Add the h2 element to the body
+document.querySelector("body").appendChild(h2);
